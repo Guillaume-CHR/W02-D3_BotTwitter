0c0a2e597495821e4735bda05d73d83a46626a35
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-16 04:34:38 +0100

    Update of gem files MTF

--- lib/gem_twitter.rb
+++ lib/gem_twitter.rb
@@ -9,12 +9,22 @@
 # 		- 
 # 		- 
 # 	Gems:
-		require 'dotenv'; Dotenv.load 
+		require 'dotenv'
+		Dotenv.load 
 		require 'rubocop'
+		require 'twitter'
 #
 #****************************************************************************
 def gem_twitter
-	
+	#Test connection
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	end
+
+	#client.update('Mon premier tweet en Ruby !!!!')
 end
 
 # End of gem_twitter .........................................................
